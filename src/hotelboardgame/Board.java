e7782517f72c8d4ed057792c6003eb213784da26
tsokkisl <l3n05.ts@gmail.com>
2018-12-21 14:37:27 +0200

    Add random board generator

--- src/hotelboardgame/Board.java
+++ src/hotelboardgame/Board.java
@@ -9,18 +9,23 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.net.URL;
+import java.util.Random;
 
 public class Board {
-    public String[] board;
+    public String[] board = new String[180];
+    public Rect[] boardgrid = new Rect[180];
     public int start;
     private static String cDir = new File("").getAbsolutePath();
     
-    public Board(){
-        board = new String[180];
-    }
     public void parseBoard() {
-        try (BufferedReader reader = new BufferedReader(new FileReader(new File(cDir + "/src/hotelboardgame/boards/simple/board.txt")))) {
+        Random rand = new Random();
+        int n = rand.nextInt(2) + 1;
+        String folder = "";
+        
+        if (n == 1) folder = "simple";
+        else folder = "default";
+        
+        try (BufferedReader reader = new BufferedReader(new FileReader(new File(cDir + "/src/hotelboardgame/boards/" + folder + "/board.txt")))) {
             String line,l = "";
             while ((line = reader.readLine()) != null) {
                 l += line;
@@ -34,7 +39,6 @@ public class Board {
                     break;
                 }
             }
-            System.out.println(start);
         } catch (IOException e) {
             e.printStackTrace();
         } 
