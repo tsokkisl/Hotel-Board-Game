5d0dc1f9ce65e31782d3e30378b8447f72108efe
tsokkisl <l3n05.ts@gmail.com>
2018-12-21 14:35:25 +0200

    Add Rect.java class

---
+++ src/hotelboardgame/Rect.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hotelboardgame;
+
+import javafx.scene.layout.StackPane;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Text;
+
+public class Rect {
+    public StackPane stack = new StackPane();
+    public Rectangle rec = new Rectangle();
+    public Text text = new Text ("");
+}
