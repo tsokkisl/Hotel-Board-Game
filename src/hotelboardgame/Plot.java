8cdc9ef82db7b70b6e023d1a71b71509e553b3be
tsokkisl <l3n05.ts@gmail.com>
2018-12-30 20:48:23 +0200

    Create Plot class

---
+++ src/hotelboardgame/Plot.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hotelboardgame;
+
+public class Plot {
+    public boolean isOwned;
+    public boolean isConstructed;
+    
+    public Plot() {
+        isOwned = false;
+        isConstructed = false;
+    }
+}
