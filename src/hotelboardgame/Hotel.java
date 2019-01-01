3d45b655f87fdb9fafec13596666911e109f2a28
tsokkisl <l3n05.ts@gmail.com>
2019-01-01 20:46:01 +0200

    Add hotel owner and update build - rent costs

--- src/hotelboardgame/Hotel.java
+++ src/hotelboardgame/Hotel.java
@@ -6,34 +6,27 @@
 package hotelboardgame;
 
 public class Hotel {
+    public Player hotelOwner;
     public String name;
     public int number;
     public int plotCost;
     public int requiredPlotCost;
     public int entranceCost;
-    public int basicBuildCost;
-    public int basicBuildRent;
-    public int[] upgradeBuildCost;
-    public int[] upgradeBuildRent;
-    public int outsideBuildCost;
-    public int maxRent;
+    public int[] buildCost;
+    public int[] rentCost;
     public int maxUpgradeLevel;
     public int currentUpgradeLevel;
     public Plot plot;
     
-    Hotel(String n, int pc, int rpc, int ec, int bc, int br, int[] ubc, int[] ubr,int obc, int mr, int num) {
+    Hotel(String n, int pc, int rpc, int ec, int[] bc, int[] br, int num) {
         name = n;
         plotCost = pc;
         requiredPlotCost = rpc;
         entranceCost = ec;
-        basicBuildCost = bc;
-        basicBuildRent = br;
-        upgradeBuildCost = ubc;
-        upgradeBuildRent = ubr;
-        outsideBuildCost = obc;
-        maxRent = mr;
+        buildCost = bc;
+        rentCost = br;
         number = num;
-        maxUpgradeLevel = ubc.length;
+        maxUpgradeLevel = bc.length;
         currentUpgradeLevel = 0;
         plot = new Plot();
     }
